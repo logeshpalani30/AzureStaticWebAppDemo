08a7c72c7d77893e09a3a3a0dec52bf8b7002f21
Logesh Palani <logesh.01@hotmail.com>
2021-03-14 01:21:19 +0530

    designed card view and changed new concept to blogging that is most use case today

---
+++ Client/Models/Paths.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Client.Models
+{
+    public class Paths
+    {
+        public static List<string> CarrerList()
+        {
+            return new List<string>() {"Logesh", "Palani", "Guna", "Meena", "Palani", "Guna", "Meena", "Logesh", "Palani", "Guna", "Meena" };
+        }
+    }
+}
