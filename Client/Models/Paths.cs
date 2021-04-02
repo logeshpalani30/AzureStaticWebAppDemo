f7d26dd1d3ba27bc5d222e07510e91e34af7c381
Logesh Palani <logesh.01@hotmail.com>
2021-04-02 21:11:27 +0530

    added workers files

--- Client/Models/Paths.cs
+++ Client/Models/Paths.cs
@@ -9,4 +9,18 @@ namespace Client.Models
             return new List<string>() {"Logesh", "Palani", "Guna", "Meena", "Palani", "Guna", "Meena", "Logesh", "Palani", "Guna", "Meena" };
         }
     }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDesc { get; set; }
+        public int ProductRating { get; set; }
+        public int OriginalPrice { get; set; }
+        public int OfferPrice { get; set; }
+        public List<string> Categories { get; set; }
+        public string OrderedCount { get; set; }
+        public List<string> ImageUrls { get; set; }
+        public int Likes { get; set; }
+        public bool IsRecommended { get; set; }
+    }
 }
---
+++ Client/Pages/IndexBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Client.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Client.Pages
+{
+    public partial class IndexBase : ComponentBase
+    {
+        public IndexBase()
+        {
+        } 
+        public List<Product> _products = new List<Product>();
+
+        private HttpClient http = new HttpClient();
+
+        protected override async Task OnInitializedAsync()
+        {
+            _products = await http.GetFromJsonAsync<List<Product>>("api/GetProducts");
+            Console.WriteLine(_products);
+        }
+
+    }
+}
